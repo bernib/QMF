79ecc6b0652a4eaf3f039eee4fc23390f67e6605
Alejandro Jarandilla <alejandroj@publiland.com.ar>
2014-04-28 11:23:25 -0300

    404 page, Nueva Clave, Resultado Busqueda.

---
+++ maqueta/nueva-clave.php
@@ -0,0 +1,72 @@
+<?php include("inc_head.php"); ?>
+
+<body>
+<div class="MainTopBanner">
+	<a href="#"><img src="images/banners/movistar-728x90.jpg" width="728" height="90" alt=""></a>
+</div>
+<?php include("inc_header.php"); ?>
+<div class="Wrapper">
+	<div class="TopBanner">
+		<div class="Desktop"><img src="images/banners/sale-960x37.png" width="960" height="37" alt=""></div>
+		<div class="Mobile"><img src="images/banners/ad-300x50.png" width="320" height="50" alt=""></div>
+	</div>
+</div>
+
+<div class="Wrapper">
+	<div class="Container">
+		<section class="NuevaClave">
+			<h2>¿Olvidaste tu contraseña?</h2>
+			<form>
+				<fieldset>
+					<p>
+						<input type="text" name="textfield6" id="textfield6" placeholder="Nueva contraseña"><br>
+						<input type="text" name="textfield7" id="textfield7" placeholder="Repetir nueva contraseña">
+					</p>
+					<input type="submit" name="button2" id="button2" value="Crear">
+				</fieldset>
+			</form>
+		</section>
+		
+		<aside class="ColSize3">
+			<div class="Ads">
+				<a href="#"><img src="images/banners/add-300x250.png" width="300" height="250" alt="CABLEVISION HD + FIBERTEL 6 MEGAS Wi-Fi $280"></a>
+			</div>
+			<div class="Estadisticas">
+				<img src="images/pics/estadisticas.png" width="300" height="375" alt="ESTADÍSTICAS">
+			</div>
+			<section class="LoMasVisto">
+				<h3>LO MÁS VISTO</h3>
+				<ul>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic4-300x165.jpg" width="300" height="165" alt=""></a></figure>
+						<h4><a href="#"><strong>1</strong> Peña pide la copa del Mundo para México</a></h4>				
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic5-300x165.jpg" width="300" height="165" alt=""></a></figure>
+						<h4><a href="#"><strong>2</strong> Zubi tendrá 60 millones más ventas para fichajes</a></h4>				
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic6-300x165.jpg" width="300" height="165" alt=""></a></figure>
+						<h4><a href="#"><strong>3</strong> Zidane: "¿El triplete? Claro que se debe hablar de ello"</a></h4>				
+					</li>
+				</ul>
+			</section>
+			<section class="Recomendados">
+				<h3>RECOMENDADOS</h3>
+				<ul>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic1-260x143.jpg" width="260" height="143" alt=""></a></figure>
+						<p><a href="#">Este viernes el Tricolor estrenará piel</a></p>
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic2-260x143.jpg" width="260" height="143" alt=""></a></figure>
+						<p><a href="#">Top 10 tenistas de todos los tiempos</a></p>
+					</li>
+				</ul>
+			</section>
+		</aside>
+	</div>
+</div>
+<?php include("inc_footer.php"); ?>
+</body>
+</html>
---
+++ maqueta/resultado-busqueda.php
@@ -0,0 +1,117 @@
+<?php include("inc_head.php"); ?>
+
+<body>
+<div class="MainTopBanner">
+	<a href="#"><img src="images/banners/movistar-728x90.jpg" width="728" height="90" alt=""></a>
+</div>
+<?php include("inc_header.php"); ?>
+<div class="Wrapper">
+	<div class="TopBanner">
+		<div class="Desktop"><img src="images/banners/sale-960x37.png" width="960" height="37" alt=""></div>
+		<div class="Mobile"><img src="images/banners/ad-300x50.png" width="320" height="50" alt=""></div>
+	</div>
+</div>
+
+<div class="Wrapper">
+	<div class="Container">
+		<section class="ResultadoBusqueda">
+			<h2>Resultado de búsqueda</h2>
+			<div class="Msg">
+				<p>Resultados <strong>1-15</strong> de <strong>1979</strong> para <strong class="Palabra">mundial</strong></p>
+			</div>
+			<ul class="Resultado">
+				<li>
+					<figure><a href="#"><span class="Fotos"><i class="fa fa-camera"></i></span><img src="images/pics/pic1-283x156.jpg" width="283" height="156" alt=""></a></figure>
+					<div class="Data">
+						<p>ABRIL 24, 2014 <i class="fa fa-clock-o"></i> 5:31 PM</p>
+						<h4><a href="#">PREMIER LEAGUE</a></h4>
+						<h3><a href="#">Peña pide la copa del mundo para México</a></h3>
+					</div>
+				</li>
+				<li>
+					<figure><a href="#"><img src="images/pics/pic2-283x156.jpg" width="283" height="156" alt=""></a></figure>
+					<div class="Data">
+						<p>ABRIL 24, 2014 <i class="fa fa-clock-o"></i> 5:31 PM</p>
+						<h4><a href="#">PREMIER LEAGUE</a></h4>
+						<h3><a href="#">El Tigre ruge en el Universitario y es  
+						campeón de Copa MX</a></h3>
+					</div>
+				</li>
+				<li>
+					<figure><a href="#"><img src="images/pics/pic1-300x165.jpg" width="285" height="156" alt=""></a></figure>
+					<div class="Data">
+						<p>ABRIL 24, 2014 <i class="fa fa-clock-o"></i> 5:31 PM</p>
+						<h4><a href="#">PREMIER LEAGUE</a></h4>
+						<h3><a href="#">Peña pide la copa del mundo para México</a></h3>
+					</div>
+				</li>
+				<li>
+					<figure><a href="#"><img src="images/pics/pic2-300x165.jpg" width="285" height="156" alt=""></a></figure>
+					<div class="Data">
+						<p>ABRIL 24, 2014 <i class="fa fa-clock-o"></i> 5:31 PM</p>
+						<h4><a href="#">PREMIER LEAGUE</a></h4>
+						<h3><a href="#">El Tigre ruge en el Universitario y es  
+						campeón de Copa MX</a></h3>
+					</div>
+				</li>
+				<li>
+					<div class="Data">
+						<p>ABRIL 24, 2014 <i class="fa fa-clock-o"></i> 5:31 PM</p>
+						<h4><a href="#">PREMIER LEAGUE</a></h4>
+						<h3><a href="#">Peña pide la copa del mundo para México</a></h3>
+					</div>
+				</li>
+				<li>
+					<div class="Data">
+						<p>ABRIL 24, 2014 <i class="fa fa-clock-o"></i> 5:31 PM</p>
+						<h4><a href="#">PREMIER LEAGUE</a></h4>
+						<h3><a href="#">El Tigre ruge en el Universitario y es  
+						campeón de Copa MX</a></h3>
+					</div>
+				</li>
+			</ul>
+		</section>
+		
+		<aside class="ColSize3">
+			<div class="Ads">
+				<a href="#"><img src="images/banners/add-300x250.png" width="300" height="250" alt="CABLEVISION HD + FIBERTEL 6 MEGAS Wi-Fi $280"></a>
+			</div>
+			<div class="Estadisticas">
+				<img src="images/pics/estadisticas.png" width="300" height="375" alt="ESTADÍSTICAS">
+			</div>
+			<section class="LoMasVisto">
+				<h3>LO MÁS VISTO</h3>
+				<ul>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic4-300x165.jpg" width="300" height="165" alt=""></a></figure>
+						<h4><a href="#"><strong>1</strong> Peña pide la copa del Mundo para México</a></h4>				
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic5-300x165.jpg" width="300" height="165" alt=""></a></figure>
+						<h4><a href="#"><strong>2</strong> Zubi tendrá 60 millones más ventas para fichajes</a></h4>				
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic6-300x165.jpg" width="300" height="165" alt=""></a></figure>
+						<h4><a href="#"><strong>3</strong> Zidane: "¿El triplete? Claro que se debe hablar de ello"</a></h4>				
+					</li>
+				</ul>
+			</section>
+			<section class="Recomendados">
+				<h3>RECOMENDADOS</h3>
+				<ul>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic1-260x143.jpg" width="260" height="143" alt=""></a></figure>
+						<p><a href="#">Este viernes el Tricolor estrenará piel</a></p>
+					</li>
+					<li>
+						<figure><a href="#"><img src="images/pics/pic2-260x143.jpg" width="260" height="143" alt=""></a></figure>
+						<p><a href="#">Top 10 tenistas de todos los tiempos</a></p>
+					</li>
+				</ul>
+			</section>
+		</aside>
+	</div>
+</div>
+<?php include("inc_footer.php"); ?>
+</body>
+</html>
